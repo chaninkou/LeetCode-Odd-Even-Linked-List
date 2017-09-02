4f027c5322bc3ca25bfad2bfaa89933d105c076d
chaninkou <michaelkoulove@yahoo.com>
2017-09-02 14:24:47 -0700

    pushing python

---
+++ lesson2.py
@@ -0,0 +1,14 @@
+import random
+import sys
+import os
+
+# global
+something = "how are you"
+def main():
+    localVariable = 3
+    print("hello you all")
+    print("my lucky number is",localVariable)
+
+main()
+print(something)
+main()
